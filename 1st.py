458373ea2a6e34ba9f2e344d9a8b1b2710d50801
Lukas Bures <burylukas@seznam.cz>
2017-07-25 17:20:51 +0200

    fixed 1st.py and update README for 1st signature

--- 1st.py
+++ 1st.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-
-# from __future__ import print_function
 import subprocess
-import sys
-import os
-import urllib
 import json
 import requests
 import common
@@ -25,29 +20,32 @@ __status__ = "Production"
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Set this parameters
+TEST_NETWORK = True  # True = use -testnet, False = mainnet
 SEND_TO_ADDRESS = "PUT_DESTINATION_ADDRESS_HERE"
 YOUR_PRIVATE_KEY = "PUT_YOUR_PRIVATE_KEY_HERE"
-UTXO_ADDRESS = "zsyF68hcYYNLPj5i4PfQJ1kUY6nsFnZkc82"
-UTXO_ADDRESS_TESTNET = "zrRBQ5heytPMN5nY3ssPf3cG4jocXeD8fm1"
 
 # how many UTXOx should be spent. Note that there is a maximum limit of 100KB per tx,
 # so effectively around 1900 UTXOs can be spent in one transaction
 # (this is a raw calculation, probably it could be more).
-SEND_AMOUNT = 10
+SEND_UTXO_AMOUNT = 10  # e.g.: 10 UTXOs is equal to (10*1,5)-TX_FEE ZENs
 TX_FEE = 0.0001
 
-ZENCLI_PATH = "./zen-cli -testnet"
-
-# Redeem script is constant to the set of signers. So don't change it unless there is a new set of signers
-YOUR_REDEEM_SCRIPT = "PUT_REDEEM_SCRIPT_HERE"
-YOUR_REDEEM_SCRIPT_TESTNET = "522103463492c6b015726cbc6bca1535acc2d4d23a2d6836f430765c5d936d49e353b0210238cab01744382b3ab77cd332f7560aee7a114ee7a07a9fe1c203ce7a2beed53c2102fa845c37c198d2d60418d9cab9ac64700662ba7a0b99b9570d4ba2c36d268e8753ae"
+if TEST_NETWORK:
+    UTXO_ADDRESS = "zrRBQ5heytPMN5nY3ssPf3cG4jocXeD8fm1"
+    ZENCLI_PATH = "./zen-cli -testnet"
+    # Redeem script is constant to the set of signers. So don't change it unless there is a new set of signers
+    YOUR_REDEEM_SCRIPT = "522103463492c6b015726cbc6bca1535acc2d4d23a2d6836f430765c5d936d49e353b0210238cab01744382b3ab77cd332f7560aee7a114ee7a07a9fe1c203ce7a2beed53c2102fa845c37c198d2d60418d9cab9ac64700662ba7a0b99b9570d4ba2c36d268e8753ae"
+    URL = "http://aayanl.tech:8081/insight-api-zen/addrs/utxo"
+else:
+    UTXO_ADDRESS = "zsyF68hcYYNLPj5i4PfQJ1kUY6nsFnZkc82"
+    ZENCLI_PATH = "./zen-cli"
+    # Redeem script is constant to the set of signers. So don't change it unless there is a new set of signers
+    YOUR_REDEEM_SCRIPT = "PUT_REDEEM_SCRIPT_HERE"
+    URL = "https://explorer.zensystem.io/insight-api-zen/addrs/utxo"
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 1a) Get all CF utxo
-url = "https://explorer.zensystem.io/insight-api-zen/addrs/utxo"
-url_testnet = "http://aayanl.tech:8081/insight-api-zen/addrs/utxo"
-
-response = requests.post(url_testnet, data={'addrs': UTXO_ADDRESS_TESTNET})
+response = requests.post(URL, data={'addrs': UTXO_ADDRESS})
 data = response.json()
 
 if response.status_code != 200:
@@ -67,15 +65,14 @@ for d in data:
         UTXO_OUTPUT_SCRIPTs.append(d["scriptPubKey"])
         UTXO_TOTAL_AMOUNT += d["amount"]
 
-        if len(UTXO_TXIDs) >= SEND_AMOUNT:
+        if len(UTXO_TXIDs) >= SEND_UTXO_AMOUNT:
             break
 
-if len(UTXO_TXIDs) != SEND_AMOUNT:
-    raise Exception("Not enough funds. Only " + str(len(UTXO_TXIDs)) + " UTXOs were found.")
+if len(UTXO_TXIDs) != SEND_UTXO_AMOUNT:
+    raise Exception("Not enough funds. Only " + str(len(UTXO_TXIDs)) + "spendable UTXOs were found.")
 
 # ----------------------------------------------------------------------------------------------------------------------
 # 2 CREATE RAW TRANSACTION
-
 # ./zen-cli
 # createrawtransaction
 # '''
@@ -140,7 +137,7 @@ for i in range(0, len(UTXO_TXIDs)):
     ARG2 += "{\"txid\": \"" + str(UTXO_TXIDs[i]) + "\", " \
             "\"vout\": " + str(UTXO_VOUTs[i]) + "," \
             "\"scriptPubKey\": \"" + str(UTXO_OUTPUT_SCRIPTs[i]) + "\"," \
-            "\"redeemScript\": \"" + str(YOUR_REDEEM_SCRIPT_TESTNET) + "\"},"
+            "\"redeemScript\": \"" + str(YOUR_REDEEM_SCRIPT) + "\"},"
 ARG2 = ARG2[:-1]  # remove comma at the end
 ARG2 += "]\'"
 
@@ -150,7 +147,7 @@ command = ZENCLI_PATH + " signrawtransaction " + ARG1 + " " + ARG2 + " " + ARG3
 
 # print command
 proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
-(out, err) = proc.communicate()
+(out, _) = proc.communicate()
 
 if len(out) == 0:
     raise Exception("Bad signrawtransaction command")
@@ -158,8 +155,7 @@ if len(out) == 0:
 out_parsed = json.loads(out)
 
 # save to the file raw tx and prepared set of outputs to sign
-json_data = {"raw_tx" : out_parsed["hex"],
-             "utxo_to_sign" : ARG2}
+json_data = {"raw_tx": out_parsed["hex"], "utxo_to_sign": ARG2}
 
 common.save(json_data, "transaction_to_sign")
 
--- README.md
+++ README.md
@@ -7,12 +7,14 @@
 - run `zend`
 - let it sync
     - if you are _1st_ signature:
+        - check `TEST_NETWORK = False` parameter, it has to be `False`, then script run on ZEN mainnet 
         - fill `SEND_TO_ADDRESS = "PUT_DESTINATION_ADDRESS_HERE"` in `1st.py`
         - fill `YOUR_PRIVATE_KEY = "PUT_YOUR_PRIVATE_KEY_HERE"` in `1st.py`
         - fill `YOUR_REDEEM_SCRIPT = "PUT_YOUR_REDEEM_SCRIPT_HERE"` in `1st.py`
+        - fill `SEND_UTXO_AMOUNT = 10` in `1st.py` (10 is only example) - that is number of UTXOs which will be spend, e.g.: 10 = (10UTXOs * 1,5CF ZENs) - TX_FEE = ammount of ZENs 
         - run `1st.py`
-        - outputs are written to **_utxo_** folder
-        - pack your **_utxo_** folder and send it to next signature
+        - outputs are written to **_next_** folder
+        - pack your **_next_** folder and send it to next signature
     - else you are _2nd_ or next signature:
         - get the **_utxo_** folder from _1st_ signature or **_next_** folder from previous signature
         - put all files to **_utxo_** folder
